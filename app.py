a41c1adbb2f92ae7720e04785e0b013617aabd15
Timothy W. Hilton <twhilton@ucsc.edu>
2020-04-14 16:04:23 -0600

    demonstrate that embedded JS bokeh objects cannot use python callbacks
    - this commit uses bokeh.embed.components to return a script and div
    representation of the latent heat quadmeshes, with the sliders to
    adjust time and (for other variables) vertical level.  This does not
    work because the dynamic plots depend on a python engine to handle
    callbacks, but the (static) generated JS does not have a python
    engine.
    
    - This proves, I think, that I really do need to run a bokeh *server*
    on a worker dyno.  The server could probably run on the web dyno,
    because heroku supports multiple processes, but this really kind of
    abuses the spirit of the free resource.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import sys
 import time
 from flask import Flask, render_template
 from yatir_visualization import get_intro_pgraph
+from surface_panels_quick_dirty import three_panel_quadmesh_compare_surface_var
 from bokeh.embed import components
 app = Flask(__name__)
 
@@ -30,12 +31,17 @@ def index():
     script = None
     while script is None:
         #time.sleep(31)
+        LH_plot = three_panel_quadmesh_compare_surface_var('LH', 'Blues')
+        sys.stdout.write('LH_plot: {}/n'.format(type(LH_plot.get_root())))
+        sys.stdout.flush()
+        script_LH, div_LH = components(LH_plot.get_root())
         script_intro, div_intro = components(get_intro_pgraph())
+        script_LH_plot, div_LH_plot = components(get_intro_pgraph())
         return(render_template('index.html',
                                script_intro=script_intro,
-                               script_LH_plot=script_intro,
+                               script_LH_plot=script_LH_plot,
                                div_intro=div_intro,
-                               div_LH_plot=div_intro))
+                               div_LH_plot=div_LH_plot))
 
 
 if __name__ == '__main__':
